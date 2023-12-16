96d2fb265663af94992c2178b0f0e56d1f29a806
alvaro <alvaro.herranz@mataro.epiaedu.cat>
2023-12-16 19:10:46 +0100

    error fix

---
+++ helpers/validateString.js
@@ -0,0 +1,11 @@
+/**
+ * @param {any} value 
+ * @returns boolean
+ */
+function validateString(value){
+    if(typeof value !== 'string' || value === "" || value === undefined){
+        return false;
+    }
+    return true;
+}
+export default validateString;
