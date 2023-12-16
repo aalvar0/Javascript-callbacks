2b1bc7face4f960cceea1f067b227c208581fa62
alvaro <alvaro.herranz@mataro.epiaedu.cat>
2023-12-16 19:42:27 +0100

    error fix

--- helpers/validateString.test.js
+++ helpers/validateString.test.js
@@ -1,24 +1,31 @@
 import validateString from "./validateString.js";
-describe("Given function validateString", () => {
-  test("Undefined value should return false", () => {
+describe("given and function", () => {
+  test("undefined: value should return false", () => {
+    // Arrange //Act// Assert
     expect(validateString(undefined)).toBe(false);
   }),
-    test("Number value should return false", () => {
-      expect(validateString(42)).toBe(false);
+    test("number: value should return false", () => {
+      // Arrange //Act// Assert
+      expect(validateString(5)).toBe(false);
     }),
-    test("Array value should return false", () => {
-      expect(validateString([1, 2, 3])).toBe(false);
+    test("array: value should return false", () => {
+      // Arrange //Act// Assert
+      expect(validateString([1, 2, 3, 4, 5])).toBe(false);
     }),
-    test("Boolean value should return false", () => {
+    test("boolean: value should return false", () => {
+      // Arrange //Act// Assert
       expect(validateString(true)).toBe(false);
     }),
-    test("Object value should return false", () => {
-      expect(validateString({ key: "value" })).toBe(false);
+    test("object: value should return false", () => {
+      // Arrange //Act// Assert
+      expect(validateString({ key: "key" })).toBe(false);
     }),
-    test("Empty string should return false", () => {
+    test("empty: string should return false", () => {
+      // Arrange //Act// Assert
       expect(validateString("")).toBe(false);
     }),
-    test("Fulfilled string should return true", () => {
-      expect(validateString("Hello, World!")).toBe(true);
+    test("fulfilled: string should return true", () => {
+      // Arrange //Act// Assert
+      expect(validateString("alvaro")).toBe(true);
     });
 });
